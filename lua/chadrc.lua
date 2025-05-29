0d1d12425f18a42bb100517f2200db771167f0b2
Nathan Lam <Nathan.lam.rt@gmail.com>
2025-05-29 20:22:53 +1000

    ching chong

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,32 @@
 -- This file needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	theme = "github_dark",
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+		-- Your transparent background overrides
+		Normal = { bg = "NONE", ctermbg = "NONE" },
+		NormalFloat = { bg = "NONE", ctermbg = "NONE" },
+		CursorLine = { bg = "#444444", ctermbg = 233 },
+		NormalNC = { bg = "NONE", ctermbg = "NONE" },
+		WinSeparator = { bg = "NONE", ctermbg = "NONE" },
+		LineNr = { bg = "NONE", ctermbg = "NONE" },
+		SignColumn = { bg = "NONE", ctermbg = "NONE" },
+		EndOfBuffer = { bg = "NONE", ctermbg = "NONE" },
+	},
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+
+M.ui = {
+	tabufline = {
+		lazyload = false
+	}
+}
 
 return M
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright" }
+local servers = { "html", "cssls", "pyright", "gopls"}
+-- Add lsp from mason to make them load 
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
