8f04495d9606d3f0793125b2c9931d2ba98a7f53
NMJlam <nathan.lam.rt@gmail.com>
2025-12-22 22:34:09 +1100

    ninja bob

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,12 +1,13 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    go = { "gofumpt" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
   format_on_save = {
-    timeout_ms = 10,
+    timeout_ms = 500,
     lsp_fallback = true,
   },
 }
---
+++ lua/custom/lsp/goLsp.lua
@@ -0,0 +1,39 @@
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+local on_init = require("nvchad.configs.lspconfig").on_init
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+
+return {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+    on_attach(client, bufnr)
+
+    -- Auto-import: organize imports on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.code_action {
+          context = { only = { "source.organizeImports" }, diagnostics = {} },
+          apply = true,
+        }
+      end,
+    })
+  end,
+  on_init = on_init,
+  capabilities = capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gotmpl", "gowork" },
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+        unusedWrite = true,
+        nilness = true,
+      },
+      completeUnimported = true,
+      usePlaceholders = true,
+      staticcheck = true,
+      semanticTokens = true,
+    },
+  },
+}
