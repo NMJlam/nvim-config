69745445ef4468536f0431f2f638061cfbd228d7
NMJlam <nathan.lam.rt@gmail.com>
2025-12-26 14:50:27 +1100

    dap for golang

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,7 +1,6 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
-local builtin = require "telescope.builtin"
 
 -- default
 map("n", ";", ":", { desc = "CMD enter command mode" })
@@ -14,6 +13,7 @@ map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 map("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
 -- live grep
+local builtin = require "telescope.builtin"
 map("n", "<C-p>", builtin.git_files, { desc = "Fuzzy Finder for git files" })
 
 -- Diagnostic
@@ -21,3 +21,17 @@ map("n", "<leader>q", vim.diagnostic.open_float, { desc = "Open diagnostic float
 
 -- Lazy git
 map("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit keybind" })
+
+-- Dap
+local dap = require "dap"
+local dapui = require "dapui"
+map("n", "<leader>dc", dap.continue, { desc = "Debug: Continue" })
+map("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+map("n", "<leader>dB", function()
+  dap.set_breakpoint(vim.fn.input "Breakpoint condition: ")
+end, { desc = "Debug: Conditional Breakpoint" })
+map("n", "<leader>di", dap.step_into, { desc = "Debug: Step Into" })
+map("n", "<leader>do", dap.step_over, { desc = "Debug: Step Over" })
+map("n", "<leader>dO", dap.step_out, { desc = "Debug: Step Out" })
+map("n", "<leader>dr", dap.run_last, { desc = "Debug: Run Last" })
+map("n", "<leader>du", dapui.toggle, { desc = "Debug: Toggle UI" })
