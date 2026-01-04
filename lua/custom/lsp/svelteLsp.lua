0ec17961859f2b25109b3257d60b5979dc770510
NMJlam <nathan.lam.rt@gmail.com>
2026-01-05 00:01:29 +1100

    asdfasdfasd

--- lua/custom/lsp/svelteLsp.lua
+++ lua/custom/lsp/svelteLsp.lua
@@ -7,6 +7,17 @@ return {
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
     on_attach(client, bufnr)
+
+    -- Auto-import: organize imports on save (same as your tsLsp.lua)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.code_action {
+          context = { only = { "source.organizeImports" }, diagnostics = {} },
+          apply = true,
+        }
+      end,
+    })
   end,
   on_init = on_init,
   capabilities = capabilities,
